b9f5e805f2b3826bb7a2a4b71df8e2547e983076
yangsua <yangsua0307@gmail.com>
2022-11-12 21:55:35 +0900

    Test 브랜치에 코드를 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main(){
+
+  int i = 0;
   printf("First OSS Code\n");
   printf("Second OSS Code\n");
+
+  for(int i =0; i<10; i++){
+    print("Test 브랜치\n");
+  }
+
 
   return 0;
 }
